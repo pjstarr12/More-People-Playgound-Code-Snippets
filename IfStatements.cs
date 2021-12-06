a941731289564dd14ec303cd93e744eb59e5f566
pjstarr12 <85199112+pjstarr12@users.noreply.github.com>
2021-12-06 15:13:42 -0800

    Create IfStatements.cs

---
+++ IfStatements.cs
@@ -0,0 +1,22 @@
+//lmao C# basics
+
+//if you want something to be true, or have a specific value, then you can use if statements
+
+//heres an example:
+if (LimbBehaviour.Health == 0f) 
+{ 
+    //code here
+}
+
+//inside the if statement, you see a "=="
+//its REQUIRED to use "==" in an if statement
+//you could use > or <
+//if you dont want something to be true then use this:
+
+if (!LimbBehaviour.IsZombie == true) //notice the "!" before we define what we want
+{ 
+    //code here
+}
+
+//theres also ">=" (greater than or equal to)
+//and "<=" (less than or equal to)
