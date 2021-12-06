39b5b796c77d655b7b7a69e9d89bfc216f54447e
pjstarr12 <85199112+pjstarr12@users.noreply.github.com>
2021-12-06 14:48:53 -0800

    Update Vector2.cs

--- Vector2.cs
+++ Vector2.cs
@@ -1,4 +1,4 @@
 //"transform" holds all the things like rotation and position and size
 
-Instance.transform.position = new Vector2(1f, -1f)//we use Vector2 because people playground is a 2d game, so there are only 2 dimensions. 0.05 is around half a pixel
-Instance.transform.localPosition += new Vector2(0.5f, 0f) //moves it to the side relative to the Instances original position
+Instance.transform.position = new Vector2(1f, -1f); //we use Vector2 because people playground is a 2d game, so there are only 2 dimensions. 0.05 is around half a pixel
+Instance.transform.localPosition += new Vector2(0.5f, 0f); //moves it to the side relative to the Instances original position
