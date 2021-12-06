bc9ee8f927b8e9258aa6951d608767726d1419fa
pjstarr12 <85199112+pjstarr12@users.noreply.github.com>
2021-12-06 15:14:39 -0800

    Update GettingAllLimbsInHuman.cs

--- GettingAllLimbsInHuman.cs
+++ GettingAllLimbsInHuman.cs
@@ -1,8 +1,8 @@
 foreach (LimbBehaviour Limb in PersonBehaviour.Limbs) //make sure PersonBehaviour is the PersonBehaviour of your human (duh)
 {
-    //if you need the GameObject, then do
+    //if you need the GameObject, then do this:
     Limb.gameObject
     //you can set all different things in LimbBehaviour (a Component [script] in the limb)
-    Limb.Health = 0f;
-    Limb.BreakingThreshold = 0
+    Limb.Health = 100000f;
+    Limb.BreakingThreshold = 1900909857f;
 }
