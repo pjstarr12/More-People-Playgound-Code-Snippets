d5ddf6e7bc2ae249fe6c4846035da5cfc7e983b2
pjstarr12 <85199112+pjstarr12@users.noreply.github.com>
2021-12-06 15:04:08 -0800

    Create IntsAndFloats.cs

---
+++ IntsAndFloats.cs
@@ -0,0 +1,6 @@
+//lmao unity and C# basics
+
+//if you are confused on what the hell an int or a float is then here:
+public int WholeNumber = 1; //an int is a whole number
+
+public float DecimalNumber = 0.5f; //a float is a decimal and HAS TO END WITH AN "f" AFTER THE NUMBER
